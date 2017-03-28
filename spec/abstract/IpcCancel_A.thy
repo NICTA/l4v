d1438a388b1ee1a292ab682558e9ce5d52a18761
Gerwin Klein <gerwin.klein@data61.csiro.au>
2017-03-28 11:08:43 +0200

    abstract: docs for notification unbinding

--- spec/abstract/IpcCancel_A.thy
+++ spec/abstract/IpcCancel_A.thy
@@ -99,12 +99,8 @@ where
         od
   od"
 
-text {* Cancel all message operations on threads queued in a notification
-endpoint. *}
-
-text {* Miscellaneous NTFN binding stuff
-FIXME! *}
-abbreviation
+text {* Notification accessors. *}
+abbreviation 
   ntfn_set_bound_tcb :: "notification \<Rightarrow> obj_ref option \<Rightarrow> notification"
 where
   "ntfn_set_bound_tcb ntfn t \<equiv> ntfn \<lparr> ntfn_bound_tcb := t \<rparr>"
@@ -114,6 +110,8 @@ abbreviation
 where
   "ntfn_set_obj ntfn a \<equiv> ntfn \<lparr> ntfn_obj := a \<rparr>"
 
+
+text {* Remove the binding between a notification and a TCB. *}
 abbreviation
   do_unbind_notification :: "obj_ref \<Rightarrow> notification \<Rightarrow> obj_ref \<Rightarrow> (unit,'z::state_ext) s_monad"
 where
@@ -123,6 +121,8 @@ where
       set_bound_notification tcbptr None
     od"
 
+
+text {* Remove bound notification from a TCB in bound state. *}
 definition
   unbind_notification :: "obj_ref \<Rightarrow> (unit,'z::state_ext) s_monad"
 where
@@ -136,6 +136,7 @@ where
        | None \<Rightarrow> return ()
    od"
 
+text {* Remove bound notification from a TCB if such notification exists. *}
 definition
   unbind_maybe_notification :: "obj_ref \<Rightarrow> (unit, 'z::state_ext) s_monad"
 where
@@ -145,6 +146,9 @@ where
        Some t \<Rightarrow> do_unbind_notification ntfnptr ntfn t
      | None \<Rightarrow> return ())
    od"
+
+
+text {* Cancel all message operations on threads queued in a notification endpoint. *}
 
 definition
   cancel_all_signals :: "obj_ref \<Rightarrow> (unit,'z::state_ext) s_monad"

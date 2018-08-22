cc8189a81faf0e29bd846bddac887186653932d4
Pang Luo <Pang.Luo@data61.csiro.au>
2018-08-22 14:11:52 +1000

    rt design: introduce four skeleton files and some cleanup
    - two new skeleton files for SchedContext and two for Reply

---
+++ spec/design/skel/ReplyDecls_H.thy
@@ -0,0 +1,22 @@
+(*
+ * Copyright 2014, General Dynamics C4 Systems
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(GD_GPL)
+ *)
+
+chapter "Function Declarations for Replies"
+
+theory ReplyDecls_H
+
+imports
+  FaultMonad_H
+
+begin
+
+#INCLUDE_HASKELL SEL4/Object/Reply.lhs decls_only
+
+end
